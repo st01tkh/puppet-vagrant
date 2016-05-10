d2591b30a02c24daeef36d7d707ec3d74b381ecf
st01tkh <st01tkh@gmail.com>
2016-05-10 23:26:09 +0300

    Fixed URL and parsing method in get_latest_vagrant_version() function

--- lib/puppet/parser/functions/get_latest_vagrant_version.rb
+++ lib/puppet/parser/functions/get_latest_vagrant_version.rb
@@ -5,6 +5,7 @@ module Puppet::Parser::Functions
   newfunction(:get_latest_vagrant_version, :type => :rvalue) do |args|
     # tags = JSON.parse(open('https://api.github.com/repos/mitchellh/vagrant/tags') { |u| u.read })
     # tags.sort_by { |tag| tag['name'] }.last['name'][1..-1]
-    JSON.parse(open('http://www.vagrantup.com/latest-version.json') { |u| u.read })["version"]
+    #JSON.parse(open('https://releases.hashicorp.com/vagrant/index.json') { |u| u.read })["version"]
+    (((JSON.parse(open('https://releases.hashicorp.com/vagrant/index.json') { |u| u.read } ))["versions"]).map {|key, val| key.dup.to_s}).map {|v| Gem::Version.new v.to_s}.max.to_s
   end
 end
